90d8506e66fd990346ad42e30ed9d5b9d63e0fd5
Ethan Davidson <theejdavidson@gmail.com>
2020-01-28 00:31:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,12 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross = 0
+  row_index = 0 
+  while row_index < diirector_data.length do
+    
+    row_index += 1
+  end
   pp director_data
 end
 
